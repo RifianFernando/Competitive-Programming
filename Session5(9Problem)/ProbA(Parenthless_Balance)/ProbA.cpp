25af0864222e8847f32ff7636c0efc9ef33dbb03
Rifian Fernando <rifianfernando19@gmail.com>
2023-10-21 17:46:45 +0700

    fix(probA): fixing problem on a

--- Session5(9Problem)/ProbA(Parenthless_Balance)/ProbA.cpp
+++ Session5(9Problem)/ProbA(Parenthless_Balance)/ProbA.cpp
@@ -10,13 +10,15 @@ bool isBalanced(const string& s) {
         // Add more mappings if needed
     };
 
+    if (s.size() % 2 == 1) return false;  // Odd length strings are not balanced
+    if (s.empty()) return true;  // Empty strings are balanced
     for (char c : s) {
-        if (c == '(' || c == '[' || c == '{') {
+        if (c == '(' || c == '[') {
             // Push opening parentheses onto the stack
             // cout << "setelah divalidasi: ";
             // cout << c << endl;
             parenthesesStack.push(c);
-        } else if (c == ')' || c == ']' || c == '}') {
+        } else if (c == ')' || c == ']') {
             // cout << "setelah divalidasi: c == ')' || c == ']' || c == '}': ";
             // cout << c << endl;
             // Check for matching opening parentheses on the stack
@@ -30,6 +32,16 @@ bool isBalanced(const string& s) {
     }
 
     return parenthesesStack.empty();  // it will return true if the stack is empty
+}
+
+bool isBalanced2(const string& s) {
+    map<char, int> parenthesesMap = {
+        {')', 0},
+        {']', 0},
+        {'(', 0},
+        {'[', 0}
+        // Add more mappings if needed
+    };
 }
 
 int main() {
