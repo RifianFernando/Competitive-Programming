9fac9f05e998244f2cf71259716e0f6db5744124
Rifian Fernando <rifianfernando19@gmail.com>
2023-10-28 12:21:37 +0700

    fix(parenthesesStack): fixing using vector

--- Session5(9Problem)/ProbA(Parenthless_Balance)/ProbA.cpp
+++ Session5(9Problem)/ProbA(Parenthless_Balance)/ProbA.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 bool isBalanced(const string& s) {
-    stack<char> parenthesesStack;
+    vector<char> parenthesesStack;
     unordered_map<char, char> parenthesesMap = {
         {')', '('},
         {']', '['}
@@ -11,23 +11,25 @@ bool isBalanced(const string& s) {
     };
 
     if (s.size() % 2 == 1) return false;  // Odd length strings are not balanced
-    if (s.empty()) return true;  // Empty strings are balanced
     for (char c : s) {
         if (c == '(' || c == '[') {
             // Push opening parentheses onto the stack
             // cout << "setelah divalidasi: ";
             // cout << c << endl;
-            parenthesesStack.push(c);
+            parenthesesStack.push_back(c);
         } else if (c == ')' || c == ']') {
             // cout << "setelah divalidasi: c == ')' || c == ']' || c == '}': ";
             // cout << c << endl;
-            // Check for matching opening parentheses on the stack
-            if (parenthesesStack.empty() || parenthesesStack.top() != parenthesesMap[c]) {
-                // cout << "setelah divalidasi: c == ')' || c == ']' || c == '}':  dan kalo ga sama: "<< endl;
-                // cout << parenthesesStack.top() << endl;
-                return false;  // Unmatched closing parentheses
+            auto it = find(parenthesesStack.begin(), parenthesesStack.end(), parenthesesMap.at(c));
+
+            if (it == parenthesesStack.end()) {
+                return false;// Unmatched closing parentheses
             }
-            parenthesesStack.pop();  // Matched, pop the opening parentheses
+            for (auto it = parenthesesStack.rbegin(); it != parenthesesStack.rend(); ++it) {
+                cout << *it << " ";
+            }
+            cout << endl;
+            // parenthesesStack.erase(it.base() - 1);  // Matched, pop the opening parentheses
         }
     }
 
